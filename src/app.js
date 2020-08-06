423fcf7ee35fa2b713ec00ae978522eba76e9be4
yuxinping <dm2267747196@qq.com>
2020-08-06 22:38:31 +0800

    第一次单元测试

--- src/app.js
+++ src/app.js
@@ -13,3 +13,61 @@ new Vue({
     }
 
 })
+import chai from 'chai'
+const expect = chai.expect
+
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {icon:'setting'}
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
+    button.$el.remove()
+    button.$destroy()
+
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {icon:'setting',
+            loading: true,
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading2')
+    button.$el.remove()
+    button.$destroy()
+
+}
+{
+    const div = document.createElement("div")
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {icon:'setting',
+            loading: true,
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let order = window.getComputedStyle(svg).order
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+
+}
+// {
+//     const Constructor = Vue.extend(Button)
+//     const gButton = new Constructor({
+//         propsData: {icon:'setting'}
+//     })
+//     gButton.$mount()
+//
+//     button.$el.remove()
+//     button.$destroy()
+//
+// }
